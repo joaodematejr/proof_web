ff60da36c8cfa193698d8af6ff4a99c06414eb2a
João Dematé Jr <joaodematejr@gmail.com>
2021-05-21 21:19:46 -0300

    Delete People OK

--- src/controllers/PeopleController.js
+++ src/controllers/PeopleController.js
@@ -4,14 +4,30 @@ const People = require('../models/People')
 module.exports = {
   async store(request, response) {
     const { firstName, lastName } = request.body;
-
     if (firstName === "") {
       return response.status(401).json({ "message": "Você precisa preencher o nome idiota!!!" });
     } else if (lastName === "") {
       return response.status(401).json({ "message": "Você precisa preencher o sobrenome idiota!!!" });
-    }else{
+    } else {
       people = await People.create({ firstName, lastName });
       return response.status(200).json(people);
     }
-  }
+  },
+
+  async show(request, response) {
+    const { firstName } = request.body;
+    const people = await People.find({ firstName })
+    if (people.length !== 0) {
+      return response.status(200).json(people);
+    }
+    return response.status(404).json({ "message": "Não existe nenhum registro no banco com esse nome idiota!!!" });
+  },
+
+  async destroy(request, response) {
+    const { id } = request.params;
+    People.findByIdAndDelete(id, function (err) {
+      if (err) return response.status(404).json({ "message": "Houve um erro  !!!" });
+      return response.status(200).json({ "message": "Deletado com Sucesso !!!" });
+    });
+  },
 };
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,7 @@ const PeopleController = require('./controllers/PeopleController');
 const routes = express.Router();
 
 routes.post("/people", PeopleController.store);
+routes.get("/people", PeopleController.show);
+routes.delete("/people/:id", PeopleController.destroy);
 
 module.exports = routes;
